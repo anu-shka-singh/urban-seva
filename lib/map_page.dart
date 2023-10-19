c05615fe5253aa1252f794cf6efa52741b5805f2
Diya <diyasingla2004@gmail.com>
2023-10-20 00:33:25 +0530

    map added

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -1,36 +1,18 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-//import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'api.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class ProblemMap extends StatefulWidget {
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
   @override
-  _ProblemMapState createState() => _ProblemMapState();
+  State<MapScreen> createState() => _MapScreenState();
 }
 
-class _ProblemMapState extends State<ProblemMap> {
-  // GoogleMapController? _controller;
-
-  // final Set<Marker> allMarkers = Set<Marker>.from([
-  //   Marker(
-  //     markerId: MarkerId('1'),
-  //     position: LatLng(37.7749, -122.4194), // Replace with actual coordinates
-  //     icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
-  //     infoWindow: InfoWindow(title: 'Water Issue'),
-  //   ),
-  //   Marker(
-  //     markerId: MarkerId('2'),
-  //     position: LatLng(37.7749, -122.4194), // Replace with actual coordinates
-  //     icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-  //     infoWindow: InfoWindow(title: 'Electricity Issue'),
-  //   ),
-  //   // Add more markers for other issues with different colors
-  // ]);
-
+class _MapScreenState extends State<MapScreen> {
   // Raw coordinates got from  OpenRouteService
   List listOfPoints = [];
 
@@ -55,23 +37,10 @@ class _ProblemMapState extends State<ProblemMap> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 500, // Set the desired height
-      width: 700, // Set the desired width
-      // child: GoogleMap(
-      //   onMapCreated: (controller) {
-      //     setState(() {
-      //       _controller = controller;
-      //     });
-      //   },
-      //   initialCameraPosition: CameraPosition(
-      //     target: LatLng(37.7749, -122.4194), // Initial map coordinates
-      //     zoom: 12.0,
-      //   ),
-      //   markers: allMarkers,
-      // ),
-      child: FlutterMap(
-        options: MapOptions(
+    return Scaffold(
+      appBar: AppBar(title: const Text("Community Issues Map")),
+      body: FlutterMap(
+        options: const MapOptions(
             initialZoom: 13, initialCenter: LatLng(6.131015, 1.223898)),
         children: [
           // Layer that adds the map
@@ -119,17 +88,14 @@ class _ProblemMapState extends State<ProblemMap> {
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blueAccent,
+        onPressed: () => getCoordinates(),
+        child: const Icon(
+          Icons.route,
+          color: Colors.white,
+        ),
+      ),
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Problem Map Example'),
-      ),
-      body: ProblemMap(),
-    ),
-  ));
 }
